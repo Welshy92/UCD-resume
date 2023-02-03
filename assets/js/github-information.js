976bac6aef730f46ebded61da19fab651402cd0e
Jacob Welsh <welshy92@outlook.com>
2023-02-03 02:13:44 +0000

    Fix spelling that caused search to not work correctly

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -2,7 +2,7 @@ function userInformationHTML(user) {
     return `
         <h2>${user.name}
             <span class="small-name">
-               (@<a href="$user.html_url" target="_blank">${user.login}</a>
+                (@<a href="${user.html_url}" target="_blank">${user.login}</a>)
             </span>
         </h2>
         <div class="gh-content">
@@ -11,40 +11,38 @@ function userInformationHTML(user) {
                     <img src="${user.avatar_url}" width="80" height="80" alt="${user.login}" />
                 </a>
             </div>
-            <p>Followers: ${user.followers} - Following: ${user.following} <br> Repos: ${user.public_repos}</p>
-        </div>`
+            <p>Followers: ${user.followers} - Following ${user.following} <br> Repos: ${user.public_repos}</p>
+        </div>`;
 }
 
 function fetchGitHubInformation(event) {
 
     var username = $("#gh-username").val();
-    if(!username) {
+    if (!username) {
         $("#gh-user-data").html(`<h2>Please enter a GitHub username</h2>`);
         return;
     }
 
     $("#gh-user-data").html(
-        `<div id="loader>
+        `<div id="loader">
             <img src="assets/css/loader.gif" alt="loading..." />
         </div>`);
 
     $.when(
-        $.getJSON(`https://api.github.com/users/$(username)`)
+        $.getJSON(`https://api.github.com/users/${username}`)
     ).then(
-        function(responce) {
-            var userData = responce;
-            $("'gh-user-data").html(userInformationHTML(userData));
-        }, function(errorResponce) {
-            if (errorResponce.status === 404) {
+        function(response) {
+            var userData = response;
+            $("#gh-user-data").html(userInformationHTML(userData));
+        },
+        function(errorResponse) {
+            if (errorResponse.status === 404) {
                 $("#gh-user-data").html(
-                    `<h2>No info found for user ${username}</h2>`
-                );
+                    `<h2>No info found for user ${username}</h2>`);
             } else {
-                console.log(errorResponce);
+                console.log(errorResponse);
                 $("#gh-user-data").html(
-                    `<h2>Error: ${errorResponce.responceJSON.message}</h2>`
-                );
+                    `<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
             }
-        }
-    );
+        });
 }
